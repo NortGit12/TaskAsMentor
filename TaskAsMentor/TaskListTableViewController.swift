9791ce83c4d2c88180d09b152b4e9f3f8f36729f
Jeff Norton <jeff12ntn@gmail.com>
2016-10-07 13:56:33 -0600

    List view editing completed

--- TaskAsMentor/TaskListTableViewController.swift
+++ TaskAsMentor/TaskListTableViewController.swift
@@ -76,17 +76,18 @@ class TaskListTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let tasks = TaskController.shared.fetchedResultsController.fetchedObjects
+                else { return }
+            TaskController.shared.remove(task: tasks[indexPath.row])
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     //==================================================
     // MARK: - Navigation
